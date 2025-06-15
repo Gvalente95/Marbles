8dc6c62311e5c32a3bb81c6d8b423686e93ec5a7
Giulio Valente <valente95giulio@gmail.com>
2025-06-15 22:22:05 +0200

    done

--- js/input.js
+++ js/input.js
@@ -75,3 +75,45 @@ function scaleDotWhilePressed() {
 	selDot.style.height = scaledSize + "px";
 	requestAnimationFrame(scaleDotWhilePressed);
 }
+
+
+addEventListener('touchstart', (event) => {
+	const touch = event.touches[0];
+	mouseX = touch.clientX;
+	mouseY = touch.clientY;
+	mousePressed = true;
+	if (keys["shift"] && !curBox)
+		curBox = init_box(mouseX, mouseY);
+}, { passive: false });
+
+addEventListener('touchend', () => {
+	mousePressed = false;
+	if (curBox) {
+		boxes.push(curBox);
+		curBox = null;
+	}
+}, { passive: false });
+
+addEventListener('touchmove', (event) => {
+	const touch = event.touches[0];
+	mouseX = touch.clientX;
+	mouseY = touch.clientY;
+
+	if (!curBox) return;
+
+	let x1 = curBox.x;
+	let y1 = curBox.y;
+	let x2 = mouseX;
+	let y2 = mouseY;
+
+	const left = Math.min(x1, x2);
+	const top = Math.min(y1, y2);
+	const width = Math.abs(x2 - x1);
+	const height = Math.abs(y2 - y1);
+	curBox.style.left = left + "px";
+	curBox.style.top = top + "px";
+	curBox.width = width;
+	curBox.height = height;
+	curBox.style.width = width + "px";
+	curBox.style.height = height + "px";
+}, { passive: false });
